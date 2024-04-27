551b9482493ad5b19dfebe3f0af985add798be4e
pg-endokenta <k.endo@4nonome.com>
2024-04-27 21:38:01 +0900

    lint

--- server/django/tasks/utils.py
+++ server/django/tasks/utils.py
@@ -1,15 +1,17 @@
-import requests
 import datetime
+
+import requests
+
+from contacts.models import Contact
 from django.conf import settings
 from django.db.models import Sum
-
-from users.models import User
-from contacts.models import Contact
 from tasks.models import Task
+from users.models import User
+
 
 def send_slack_message():
     now = datetime.datetime.now()
-    now = now.strftime('%Y-%m-%d %H:%M:%S')
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
     contact_count = Contact.objects.count()
     user_count = User.objects.count()
     tasks = Task.objects.all()
@@ -18,7 +20,7 @@ def send_slack_message():
     task_failed_count = tasks.filter(status=Task.FAILED).count()
 
     # タスクの金額情報
-    total_fine_in_progress = tasks.filter(status=Task.IN_PROGRESS).aggregate(total_fine=Sum("fine"))[ "total_fine"]
+    total_fine_in_progress = tasks.filter(status=Task.IN_PROGRESS).aggregate(total_fine=Sum("fine"))["total_fine"]
     total_fine_done = tasks.filter(status=Task.DONE).aggregate(total_fine=Sum("fine"))["total_fine"]
     total_fine_failed = tasks.filter(status=Task.FAILED).aggregate(total_fine=Sum("fine"))["total_fine"]
 
@@ -27,37 +29,32 @@ def send_slack_message():
     total_fine_failed = total_fine_failed if total_fine_failed else 0
 
     token = settings.SLACK_API_TOKEN
-    channel = '#継続or罰金'
+    channel = "#継続or罰金"
     text_list = [
-        f' ============================= ',
-        f'現在時刻:{now}',
-        f'=== 継続or罰金 定期報告 ===',
-        f'問い合わせ数　　　　: {contact_count}',
-        f'ユーザー数　　　　　: {user_count}',
-        f'総タスク数　　　　　: {tasks.count()}',
-        f'　完了タスク数　　　: {task_done_count}',
-        f'　完了タスクの金額　: ¥{total_fine_done:,.0f}',
-        f'　進行中タスク数　　: {task_in_progress_count}',
-        f'　進行中タスクの金額: ¥{total_fine_in_progress:,.0f}',
-        f'　失敗タスク数　　　: {task_failed_count}',
-        f'　失敗タスクの金額　: ¥{total_fine_failed:,.0f}',
+        " ============================= ",
+        f"現在時刻:{now}",
+        "=== 継続or罰金 定期報告 ===",
+        f"問い合わせ数　　　　: {contact_count}",
+        f"ユーザー数　　　　　: {user_count}",
+        f"総タスク数　　　　　: {tasks.count()}",
+        f"　完了タスク数　　　: {task_done_count}",
+        f"　完了タスクの金額　: ¥{total_fine_done:,.0f}",
+        f"　進行中タスク数　　: {task_in_progress_count}",
+        f"　進行中タスクの金額: ¥{total_fine_in_progress:,.0f}",
+        f"　失敗タスク数　　　: {task_failed_count}",
+        f"　失敗タスクの金額　: ¥{total_fine_failed:,.0f}",
     ]
 
-    text = '\n'.join(text_list)
-
+    text = "\n".join(text_list)
 
     # Slack APIのURL
-    url = 'https://slack.com/api/chat.postMessage'
+    url = "https://slack.com/api/chat.postMessage"
 
     # ヘッダとデータを用意
-    headers = {'Authorization': f'Bearer {token}'}
+    headers = {"Authorization": f"Bearer {token}"}
     data = {
-        'channel': channel,
-        'text': text,
+        "channel": channel,
+        "text": text,
     }
-
     # POSTリクエストを送信
-    try:
-        requests.post(url, headers=headers, data=data, timeout=10)
-    except:
-        pass
+    requests.post(url, headers=headers, data=data, timeout=10)
