3d1ce4662eda7c17f923e1524f0d7d3baa4ae670
pg-endokenta <k.endo@4nonome.com>
2024-05-04 13:01:59 +0900

    タスク成功時に自動でオーソリがキャンセルされるように

---
+++ server/django/payments/utils.py
@@ -0,0 +1,23 @@
+import stripe
+
+from django.conf import settings
+
+from tasks.models import Task
+
+from .models import Payment
+
+
+# オーソリをキャンセルする関数
+def cancel_authorization(task_id):
+    task = Task.objects.get(id=task_id)
+    
+    # 罰金が0の場合は何もしない
+    if task.fine == 0:
+        return
+    
+    payment = Payment.objects.get(task=task)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    stripe.PaymentIntent.cancel(payment.payment_intent_id)
+
+
+
